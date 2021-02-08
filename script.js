5ba422a359e92adb89fc4d4b46a2792ca7a315cc
vimal Raveendran <vimalraveendra@gmail.com>
2021-02-08 15:26:35 +0100

    updated index dot html file to render the details of the github profiles & also updated the style sheet

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const API_URL =`https://api.github.com/users/`
 
+const formEl = document.getElementById('form')
+const mainEl = document.getElementById('main')
+const searchEL=document.getElementById('search')
+
 async function getUsers(url){
-   const resp = await fetch(url+"vimalKumar");
+   const resp = await fetch(url+"Christopher James");
    const userData = resp.json()
    console.log("user",userData)
 }
